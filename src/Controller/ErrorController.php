6950c8874308f5c4ab3ea7857752bb8c63b33cfc
Clemence <38Clem@users.noreply.github.com>
2020-04-14 10:58:40 +0200

    start session in error controller

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 class ErrorController {
 
     public function error404 () {
+        session_start();
         header("HTTP/1.1 404 Not Found");
         include __DIR__ . "/../../templates/error/not-found.html.php";
     }
 
     public function error500 () {
+        session_start();
         header("HTTP/1.1 500 Internal Error");
         include __DIR__ . "/../../templates/error/internalError.html.php";
     }
--- src/Controller/PasswordController.php
+++ src/Controller/PasswordController.php
@@ -6,6 +6,7 @@ class PasswordController
 
     public function forgotPassword()
     {
+        session_start();
         echo 'Forgot Password, send email pls';
     }
 }

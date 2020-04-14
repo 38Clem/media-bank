640d5624182b61bd3e09af6ad114a32845c27f06
Clemence <38Clem@users.noreply.github.com>
2020-04-14 10:46:24 +0200

    Add condition for displaying link in nav-bar

--- templates/_header.html.php
+++ templates/_header.html.php
@@ -16,6 +16,7 @@
     </button>
     <div class="collapse navbar-collapse " id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto ">
+            <?php if(!array_key_exists("user", $_SESSION)) {?>
             <li class="nav-item">
                 <a class="nav-link" href="/login">
                     <img class="img-fluid" src="/assets/images/log-in.png" style="height: 5vh; width: 10vw;"/>
@@ -26,11 +27,22 @@
                     <img class="img-fluid" src="/assets/images/sign-up.png" style="height: 5vh; width: 10vw;"/>
                 </a>
             </li>
+            <?php } else{ ?>
+                <li>
+                   <a href="/profile"> <?php echo $_SESSION["user"]->getName()?> </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="/logout">
+                        <h2 >Log Out</h2>
+                    </a>
+                </li>
+            <?php } ?>
             <li class="nav-item">
                 <a class="nav-link" href="/search">
                     <img class="img-fluid" src="/assets/images/search.png" style="height: 5vh; width: 10vw;"/>
                 </a>
             </li>
+
 <!--           navBar collapse-->
             <li>
                 <a class="nav-link" href="/login">
